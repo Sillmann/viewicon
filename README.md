790bad7233e943ccebf1a9ad914932251593e7e0
ssilman <ssilman@gmail.com>
2023-02-09 20:59:39 -0300

    Exemplo para incluir um icone atraves do feathericon

---
+++ README.md
@@ -0,0 +1,10 @@
+# ViewIcon
+
+1. npm install react-icons
+
+2. import {FiLogIn} from 'react-icons/fi';
+
+]### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {FiKey} from 'react-icons/fi';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          <FiKey size={160} color="#000"/>
+          View Icon in <a href="https://feathericons.com/">feathericons</a>
+        </p>
+    </div>
+
+  );
+}
+
+export default App;
